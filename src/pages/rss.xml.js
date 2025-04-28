0508452c42f7b9cd0ad31125a0afc73228804da1
aapouncy <aapouncy@fortlewis.edu>
2025-04-27 21:10:20 -0600

    Fixed stuff

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,8 +4,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function GET(context) {
   return rss({
     // Your blog’s title and description
-    title:       'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title:       'Ashriel & Aslan Pouncy | Blog',
+    description: 'Our journey learning Astro',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
